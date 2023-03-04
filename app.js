8532e65b6b3a3405bd4ee7ef5baf7270c3526050
osamaabdulazeem <121614156+osamaabdulazeem@users.noreply.github.com>
2023-03-04 19:57:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+// var arr = [21,22,23,25,26,27,28,29]
+// var a = arr[i];
+// for(var i=0; i<arr.length; i++){
+   
+//     if(arr[i+1] -  arr[] ){
+       
+//         console.log(arr[i]);
+//     }
+
+// }
+// document.write(arr);
+
+
+
+var arr = ["hdg835sd45split"];
+
+document.write(arr);
+// arr.split("");
+
+// var charCode = char.charCodeAt();
+ for (var i = 0; i<arr.length-1; i++) {
+     if (charCode >= 97 && charCode <= 122){
+        console.log();
+     }
+ }
+
+
+// console.log(arr.sort());
+
+// console.log(arr.split (""));
+// document.write(arr);
+// str.charCodeAt();
+// console.log(str);
+
+// mix = alpha.split("");
+// str.split("-");
+// console.log (str);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
